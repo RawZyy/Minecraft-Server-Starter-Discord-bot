47438c8a6e44bca58a4f533bad47e10cb50cde70
RawZyy <100104706+RawZyy@users.noreply.github.com>
2022-09-11 02:34:05 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({intents : [GatewayIntentBits.Guilds , GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
+const token = "HIDDENTOKEN";
+const PREFIX = '-';
+const child_process = require('child_process');
+
+client.once('ready', () => {
+    console.log('Le bot est en ligne! ;)')
+});
+
+client.on('messageCreate', message=>{
+    console.log(message.content)
+    if(!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+    const args = message.content.slice(PREFIX.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'start'){
+        console.log('Starting MC server');
+        message.channel.send('Starting Minecraft Server...');
+        child_process.execFile('FILENAME');
+    }
+});
+client.login(token);
