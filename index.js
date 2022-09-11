324472a1638eac9937189c967d8b86d63763d1b4
RawZyy <100104706+RawZyy@users.noreply.github.com>
2022-09-11 02:48:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({intents : [GatewayIntentBits.Guilds , GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
-const token = "HIDDENTOKEN"; //change HIDDENTOKEN to your bot's token
+const token = "HIDDENTOKEN"; //replace HIDDENTOKEN with your bot's token
 const PREFIX = '-'; //change this if you want to use another prefix
 const child_process = require('child_process');
 
@@ -18,7 +18,7 @@ client.on('messageCreate', message=>{
     if(command === 'start'){
         console.log('Starting MC server');
         message.channel.send('Starting Minecraft Server...');
-        child_process.execFile('FILENAME'); //change FILENAME to your actual .bat filename
+        child_process.execFile('FILENAME'); //replace FILENAME with your actual .bat filename
     }
 });
 client.login(token);
