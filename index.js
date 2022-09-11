138ed3c4a692ed548330dbdd1eb60b72a9d0220e
RawZyy <100104706+RawZyy@users.noreply.github.com>
2022-09-11 04:04:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 const client = new Client({intents : [GatewayIntentBits.Guilds , GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
 const token = "HIDDENTOKEN"; //replace HIDDENTOKEN with your bot's token
 const PREFIX = '-'; //change this if you want to use another prefix
 const child_process = require('child_process');
 
 client.once('ready', () => {
+    client.user.setActivity("Made by RawZ", { type: ActivityType.Playing});
     console.log('The bot is online! ;)')});
 
 cclient.on('messageCreate', message=>{
